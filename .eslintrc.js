a488ffca5ef5f03d50bda781962473f5fbaeed26
Sanya210904 <sanya210904@gmail.com>
2024-10-24 16:59:18 +0300

    add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'off',
+    'import/no-named-as-default': 'off',
+    'import/named': 'off',
+    'import/namespace': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
